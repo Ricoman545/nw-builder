99147501bc193f89566f481d12344b57b5c79fa3
Moises Dobarganes <designerexpert@gmail.com>
2025-01-24 14:27:02 -0500

    adjusted properties in artifacts json and created dropdown on main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import arrayOfArtifacts from './artifacts.json';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  console.log('Array of Artifacts', arrayOfArtifacts);
+  const alphabeticalSortFunction = (a, b) => a.name.localeCompare(b.name);
+
+  const sortedArrayOfArtifacts = arrayOfArtifacts.sort(alphabeticalSortFunction);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>NW Builder</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div style={{ width: '100%', height: '80vh', border: '2px solid black', overflowY: 'scroll' }}>
+        <label htmlFor="artifact">Pick your Artifact</label>
+        <select id="artifact">
+          {sortedArrayOfArtifacts.map(artifact => {
+            const artifactDamage = artifact.damage ? artifact.damage : '';
+            const artifactDamageType = artifact.damageType ? artifact.damageType : '';
+            return (
+              <option
+                style={{ width: '100%' }}
+                key={artifact.name}
+                value={`${artifact.name}`}
+              >{`${artifact.name} ${artifact.slot} ${artifactDamage} ${artifactDamageType}`}</option>
+            );
+          })}
+        </select>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
